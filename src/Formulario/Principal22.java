3e8d23f757c050178d094f18749a88923025f088
GODEZ <GODEZ@10.18.200.78>
2020-03-17 14:52:15 -0600

    diseñanado la interfaz. (en proceso) cambie el fondo de la pantalla y elimine y volvi a agregar texto, botone y jScrollPane. pero al ejecutar me aparece la pantalla completa....

--- src/Formulario/Principal22.java
+++ src/Formulario/Principal22.java
@@ -9,12 +9,12 @@ package Formulario;
  *
  * @author GODEZ
  */
-public class Principal extends javax.swing.JFrame {
+public class Principal22 extends javax.swing.JFrame {
 
     /**
      * Creates new form Principal
      */
-    public Principal() {
+    public Principal22() {
         initComponents();
     }
 //fghjkjhgfgh
@@ -88,20 +88,21 @@ public class Principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal22.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal22.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal22.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Principal22.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Principal().setVisible(true);
+                new Principal22().setVisible(true);
             }
         });
     }
